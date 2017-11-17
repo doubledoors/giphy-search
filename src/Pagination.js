b12f384becd3166310f7c2ef0bc94763a4f14678
Ben Sharp <bfwsharp@gmail.com>
2017-11-17 14:55:04 +0000

    Fix issue where pagination buttons could be clicked outside the button area

--- src/Pagination.js
+++ src/Pagination.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 const Pagination = ({ page, pageCount, onPageClick }) => {
   return (
     <div className="Pagination">
-      <span onClick={() => onPageClick()}>
-        <i className="fa fa-chevron-left fa-2x Pagination__button Pagination__button--prev" aria-hidden="true"></i>
-      </span>
+      <div className="Pagination__button Pagination__button--prev" onClick={() => onPageClick()}>
+        <i className="fa fa-chevron-left fa-2x" aria-hidden="true"></i>
+      </div>
       <span className="Pagination__display">Page {page} of {pageCount}</span>
-      <span onClick={() => onPageClick(true)}>
-        <i className="fa fa-chevron-right fa-2x Pagination__button Pagination__button--next" aria-hidden="true"></i>
-      </span>
+      <div className=" Pagination__button Pagination__button--next" onClick={() => onPageClick(true)}>
+        <i className="fa fa-chevron-right fa-2x" aria-hidden="true"></i>
+      </div>
     </div>
   );
 };
