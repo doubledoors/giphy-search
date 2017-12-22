fddc4ebbe89133745f6fb9ae9e2cc0cec5eaba12
Ben Sharp <bfwsharp@gmail.com>
2017-12-22 02:23:42 +0000

    Add image preloading and spinner

--- src/Gif.js
+++ src/Gif.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const Gif = ({ src, id, onGifClick }) => {
-  return (
-    <img className="Gif fadeIn" src={src} onClick={() => onGifClick(id)} alt="" />
-  );
-};
+class Gif extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageStatus: 'loading' };
+  }
+
+  onImageLoaded() {
+    this.setState({ imageStatus: 'loaded' });
+  }
+
+  onImageError() {
+    this.setState({ imageStatus: 'failed to load' });
+  }
+
+  render() {
+
+    let classes = classnames('Gif', {
+      'fadeIn': this.state.imageStatus === "loaded"
+    });
+
+    return (
+      <div>
+        <img
+          className={classes}
+          src={this.props.src}
+          onLoad={this.onImageLoaded.bind(this)}
+          onError={this.onImageError.bind(this)}
+          onClick={() => this.props.onGifClick(this.props.id)}
+          alt=""
+        />
+        {this.state.imageStatus === 'loading' &&
+          <i class="fa fa-spinner spin" aria-hidden="true"></i>
+        }
+      </div>
+    );
+  }
+}
 
 Gif.propTypes = {
   src: PropTypes.string.isRequired,
